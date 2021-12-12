69a63dcc12b2ee1ee5b0feb95a83d4652a570068
Richard Klein <rich@neovive.com>
2021-12-12 13:45:14 -0500

    customized config files

--- Brewfile
+++ Brewfile
@@ -7,7 +7,7 @@ tap 'nicoverbruggen/homebrew-cask'
 tap 'stripe/stripe-cli'
 
 # Binaries
-brew 'awscli'
+#brew 'awscli'
 brew 'bash' # Latest Bash version
 brew 'coreutils' # Those that come with macOS are outdated
 brew 'ffmpeg'
@@ -17,7 +17,6 @@ brew 'gnupg'
 brew 'grep'
 brew 'httpie'
 brew 'mackup'
-brew 'mas' # Mac App Store manager
 brew 'pkg-config' # https://github.com/driesvints/dotfiles/issues/20
 brew 'stripe/stripe-cli/stripe'
 brew 'stripe/stripe-mock/stripe-mock'
@@ -32,8 +31,8 @@ brew 'gifsicle'
 
 # Development
 brew 'php'
-brew 'php@8.0'
-brew 'php@7.4'
+brew 'php@8.1'
+brew 'php@7.2'
 brew 'composer'
 brew 'imagemagick'
 brew 'meilisearch'
@@ -41,39 +40,27 @@ brew 'mysql'
 brew 'nginx'
 brew 'node'
 brew 'redis'
-brew 'yarn'
 
 # Apps
 cask '1password'
-cask 'caffeine'
-cask 'dbngin'
 cask 'discord'
-cask 'docker'
-cask 'figma'
-cask 'firefox'
-cask 'github'
 cask 'google-chrome'
-cask 'gpg-suite'
-cask 'helo'
-cask 'homebrew/cask-drivers/elgato-stream-deck'
-cask 'imageoptim'
-cask 'insomnia'
-cask 'pastebot'
-cask 'phpmon'
-cask 'phpstorm'
-cask 'ray'
-cask 'screenflow'
-cask 'signal'
-cask 'slack'
-cask 'tableplus'
-cask 'telegram-desktop'
-cask 'the-unarchiver'
-cask 'tinkerwell'
-cask 'transmit'
-cask 'tunnelbear'
-cask 'tuple'
 cask 'visual-studio-code'
+cask 'helo'
+cask 'imageoptim'
+cask 'signal'
+cask 'tableplus'
 cask 'zoom'
+cask 'notion'
+cask 'karabiner-elements'
+cask 'moom'
+cask 'witch'
+cask 'camtasia'
+cask 'snagit'
+cask 'appcleaner'
+cask 'invoker'
+
+
 
 # Quicklook
 cask 'qlmarkdown'
@@ -87,13 +74,7 @@ cask 'font-source-code-pro-for-powerline'
 cask 'font-source-code-pro'
 cask 'font-source-sans-pro'
 cask 'font-source-serif-pro'
-
-# Mac App Store
-mas 'Byword', id: 420212497
-mas 'Giphy Capture', id: 668208984
-mas 'Keynote', id: 409183694
-mas 'Numbers', id: 409203825
-mas 'Spark', id: 1176895641
-mas 'Speedtest', id: 1153157709
-mas 'Things', id: 904280696
-mas 'Tweetbot', id: 1384080005
+cask 'font-cascadia-code'
+cask 'font-cascadia-code-pl'
+cask 'font-cascadia-mono'
+cask 'font-cascadia-mono-pl'
--- aliases.zsh
+++ aliases.zsh
@@ -1,13 +1,15 @@
 # Shortcuts
-alias copyssh="pbcopy < $HOME/.ssh/id_ed25519.pub"
+#alias copyssh="pbcopy < $HOME/.ssh/id_ed25519.pub"
+alias showpublickey='cat ~/.ssh/id_rsa.pub'
+alias copyssh="pbcopy < ~/.ssh/id_rsa.pub"
 alias reloadshell="source $HOME/.zshrc"
 alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
 alias ll="/opt/homebrew/opt/coreutils/libexec/gnubin/ls -AhlFo --color --group-directories-first"
-alias phpstorm='open -a /Applications/PhpStorm.app "`pwd`"'
 alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
 alias c="clear"
 alias compile="commit 'compile'"
 alias version="commit 'version'"
+alias localip="ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'"
 
 # Directories
 alias dotfiles="cd $DOTFILES"
@@ -15,11 +17,20 @@ alias library="cd $HOME/Library"
 alias sites="cd $HOME/Sites"
 alias lara="sites && cd laravel/"
 alias docs="lara && cd docs/"
+alias ll='ls -FGlAhp'
+alias ..="cd ../"
+alias ...="cd ../../"
+alias ....="cd ../../../"
+alias .....="cd ../../../../"
 
 # Laravel
 alias a="php artisan"
 alias fresh="php artisan migrate:fresh --seed"
 alias seed="php artisan db:seed"
+alias pu="vendor/bin/phpunit"
+alias puf="vendor/bin/phpunit --filter "
+alias art="php artisan"
+alias migrate="php artisan migrate"
 
 # PHP
 alias cfresh="rm -rf vendor/ composer.lock && composer i"
@@ -31,9 +42,6 @@ alias watch="npm run watch"
 
 # Docker
 alias docker-composer="docker-compose"
-
-# SQL Server
-alias mssql="docker run -e ACCEPT_EULA=Y -e SA_PASSWORD=LaravelWow1986! -p 1433:1433 mcr.microsoft.com/mssql/server:2017-latest"
 
 # Git
 alias gst="git status"
@@ -51,4 +59,11 @@ alias push="git push"
 alias resolve="git add . && git commit --no-edit"
 alias stash="git stash -u"
 alias unstage="git restore --staged ."
-alias wip="commit wip"
+alias wip="git add . && git commit -m 'wip'"
+alias nah="git reset --hard && git clean -df"
+alias gs="git status"
+
+# Other
+alias aliases="cat ~/aliases | grep alias"
+alias notes="cd ~/notes && code ."
+alias notes-save="git add . && git commit -m 'updated notes' && git push"
